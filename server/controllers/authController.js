0b8e38b5e73d11748b9ab3d690999f6377a93db2
Your Name <oreoluwaonabajomail@gmail.com>
2025-07-23 05:23:49 +0100

    update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,7 +15,7 @@ const signin = async (req, res) => {
 			return res.status(401).json({ msg: "Invalid Credentials" });
 		}
 		const token = jwt.sign(
-			{ _id: user._id, username: user.username, email: user.email, isAdmin: user.isAdmin },
+			{ id: user._id, username: user.username, email: user.email, isAdmin: user.isAdmin },
 			process.env.JWT_SECRET_KEY,
 			{ expiresIn: "1h" }
 		);
